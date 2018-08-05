da49682361c7580a0f5d01c2f2fd391756d0a611
TheEndurance <rawa.jalal@gmail.com>
2018-08-04 23:31:57 -0400

    large commit of setup and model and db

---
+++ db/notes-memory.js
@@ -0,0 +1,43 @@
+const Note = require('../models/Notes.js');
+
+const notes = [];
+
+exports.update = exports.create = function(key,title,body){
+    return new Promise((resolve,reject)=>{
+        notes[key] = new Note(key,title,body);
+        resolve(notes[key]);
+    });
+}
+
+exports.read = function(key){
+    return new Promise((resolve,reject)=>{
+        if (notes[key]){
+            resolve(notes[key]);
+        } else {
+            reject(`Note with key '${key}' does not exist.`);
+        }
+    });
+}
+
+exports.destroy = function(key){
+    return new Promise((resolve,reject)=>{
+        if (notes[key]){
+            delete notes[key];
+            resolve();
+        } else {
+            reject(`Note with key '${key} does not exist.'`);
+        }
+    });
+}
+
+exports.keylist =  function(){
+    return new Promise((resolve,reject)=>{
+        resolve(Object.keys(notes));
+    });
+}
+
+exports.count =  function(){
+    return new Promise((resolve,reject)=>{
+        resolve(notes.length);
+    });
+}
