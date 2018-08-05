5c72c08ff3fcbb38f525cade7b0d9c39062963a3
TheEndurance <rawa.jalal@gmail.com>
2018-08-05 01:38:27 -0400

    note routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const notes = require('./routes/notes.js');
 
 const app = express();
 
+app.use('/notes',notes);
---
+++ routes/notes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = exports.router = express.Router();
+
+//get the note
+router.get('/notes/:key',async (req,res,next)=>{
+
+});
+
+//create
+router.post('/notes',async (req,res,next)=>{
+
+});
+
+//update
+router.put('/notes/:key',async (req,res,next)=>{
+
+});
+
+//delete
+router.delete('/notes/:key', async (req,res,next)=> {
+
+});
+
+
